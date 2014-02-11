aa10e5943054e018a1e690bd032710f45ce766f9
Myles Borins <myles.borins@gmail.com>
2014-02-11 14:37:05 -0800

    Yeyeyeyye

---
+++ BirdSong/core/TouchCollectionEntity.h
@@ -0,0 +1,14 @@
+//
+//  TouchCollectionEntity.h
+//  BirdSong
+//
+//  Created by thealphanerd on 2/11/14.
+//  Copyright (c) 2014 Ge Wang. All rights reserved.
+//
+
+#ifndef __BirdSong__TouchCollectionEntity__
+#define __BirdSong__TouchCollectionEntity__
+
+#include <iostream>
+
+#endif /* defined(__BirdSong__TouchCollectionEntity__) */
---
+++ BirdSong/core/TouchCollectionEntity.mm
@@ -0,0 +1,9 @@
+//
+//  TouchCollectionEntity.cpp
+//  BirdSong
+//
+//  Created by thealphanerd on 2/11/14.
+//  Copyright (c) 2014 Ge Wang. All rights reserved.
+//
+
+#include "TouchCollectionEntity.h"
